f14bd8e0e7faf3d5ab9610530f776abd4f1c3ebc
Alaa Ahmed Youssef <master_here100@yahoo.com>
2020-02-23 23:24:19 +0200

    made .. Done .. this project was only for using new features in c#

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,12 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
+        /*
         public ViewResult Index()
         {
-            return View(new string[] { "C#", "Language", "Features" });
+            /*
+            List<string> results = new List<string>();
+            
+            foreach(Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                string category = p?.Category ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add($"Name: {name}, Category: {category}, Price: {price}, Related: {relatedName}");
+            }
+            return View(results);
+            */
+        /*
+        ShoppingCart cart
+                  = new ShoppingCart { Products = Product.GetProducts() };
+        decimal cartTotal = cart.TotalPrices();
+        return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+    */
+        /*   // return View(Product.GetProducts().Select(p => p?.Name));
+           var products = new[] {
+               new { Name = "Kayak", Price = 275M },
+               new { Name = "Lifejacket", Price = 48.95M },
+               new { Name = "Soccer ball", Price = 19.50M },
+               new { Name = "Corner flag", Price = 34.95M },
+               };
+           //return View(products.Select(p => p?.Name));
+           return View(products.Select(p => p.GetType().Name));
+       }
+       */
+        /*
+         public async Task<ViewResult> Index()
+         {
+             long? length = await MyAsyncMethods.GetPageLength();
+             return View(new string[] { $"Length: {length}" });
+         }
+         */
+
+        public ViewResult Index()
+        {
+            var products = new[] {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+                };
+            return View(products.Select(p =>
+            $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
         }
     }
 }
---
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public async static Task<long?> GetPageLength()
+        {
+            /*
+            HttpClient client = new HttpClient();
+            var httpTask = client.GetAsync("http://apress.com");
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) => {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+            equals this */
+            
+            HttpClient client = new HttpClient();
+            var httpMessage = await client.GetAsync("http://apress.com");
+            return httpMessage.Content.Headers.ContentLength;
+            
+        }
+    }
+}
